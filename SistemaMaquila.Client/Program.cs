0714f1903d86d49c9eec2ac01ac00a4d94075168
Augusto Ojeda Notebook <manuojedauniversidad@gmail.com>
2026-06-12 16:54:25 -0300

    Modificaciones Docker 2

--- SistemaMaquila.Client/Program.cs
+++ SistemaMaquila.Client/Program.cs
@@ -1,22 +1,22 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
-using SistemaMaquila.Client;
-using SistemaMaquila.Client.Extensiones;
-using SistemaMaquila.Client.Servicios;
+    using Microsoft.AspNetCore.Components.Web;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using MudBlazor.Services;
+    using SistemaMaquila.Client;
+    using SistemaMaquila.Client.Extensiones;
+    using SistemaMaquila.Client.Servicios;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
 
-string desarrollo = "https://localhost:7041"; 
+    string desarrollo = "https://localhost:7041"; 
 
-builder.Services.AddMudServices();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(desarrollo) });
-builder.Services.AddScoped<IRepositorio, Repositorio>();
-builder.Services.AddScoped<EmpleadoService>();
-builder.Services.AddScoped<LineaService>();
-builder.Services.AddScoped<OperacionService>();
-builder.Services.AddScoped<TipoMaquinaService>();
+    builder.Services.AddMudServices();
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(desarrollo) });
+    builder.Services.AddScoped<IRepositorio, Repositorio>();
+    builder.Services.AddScoped<EmpleadoService>();
+    builder.Services.AddScoped<LineaService>();
+    builder.Services.AddScoped<OperacionService>();
+    builder.Services.AddScoped<TipoMaquinaService>();
 
-await builder.Build().RunAsync();
+    await builder.Build().RunAsync();
